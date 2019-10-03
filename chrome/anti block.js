7015c066b169551103da6ba4de076b2baddf46d7
zGit <franc.rank@gmail.com>
2019-10-03 13:22:53 +0200

    fixed

---
+++ chrome/anti block.js
@@ -0,0 +1,6 @@
+document.getElementById("content-wrapper").className = "wrapper content-wrapper"
+var element = document.getElementById("vue-default-module")
+element.parentNode.removeChild(element);
+console.log("Wyjebany")
+
+
